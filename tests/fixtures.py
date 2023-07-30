4f171ba8192ff212ffe458745f3f781621872fcb
PaulinaPacyna <pacyna1998@gmail.com>
2023-07-30 22:31:50 +0200

    added annotations added some tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,11 +1,6 @@
-import pathlib
-
 from pytest import fixture
 
 
 @fixture
-def variable_def(tmp_path: pathlib.Path):
-    CONTENT = "def f1(a, b: int):\n    return 5"
-    file = tmp_path / "file.py"
-    file.write_text(CONTENT, encoding="utf-8")
-    return file
+def mixed():
+    return "def f1(a, b: int):\n    return 5"
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,40 @@
+import pytest
+
 from annotation_checker.main import check_annotated
-from tests.fixtures import *
+import pathlib
+from pytest import fixture
 
 
-def test_check_annotated(variable_def):
-    assert check_annotated([variable_def])
+@fixture
+def mixed_args():
+    return "def f1(a, b: int):\n    return 5"
+
+
+@fixture
+def no_return():
+    return "def f1(a: int):\n    pass"
+
+
+@fixture
+def no_args():
+    return "def f1() -> None:\n    pass"
+
+
+@fixture
+def not_a_function():
+    return "x=4"
+
+
+@pytest.mark.parametrize(
+    "input,result",
+    [
+        ("mixed_args", False),
+        ("no_return", False),
+        ("no_args", True),
+        ("not_a_function", True),
+    ],
+)
+def test_check_annotated(input, result, tmp_path: pathlib.Path, request):
+    file = tmp_path / "file.py"
+    file.write_text(request.getfixturevalue(input), encoding="utf-8")
+    assert check_annotated([file]) == result
