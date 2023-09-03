daf24e40f5e01ab6161ea3dcdef161995021837f
PaulinaPacyna <pacyna1998@gmail.com>
2023-09-03 17:34:10 +0200

    added docstrings, extraxted parse_arguments method

--- annotation_checker/checkers.py
+++ annotation_checker/checkers.py
@@ -6,13 +6,31 @@ from typing import List, Optional
 
 class Checker(ABC):
     def __init__(self) -> None:
-        self.errors = []
+        self.errors = []  # TODO: make this field private
 
     @abstractmethod
     def check(self) -> bool:
+        """
+        Returns True if a given function/method is type-annotated according to settings.
+        Returns
+        -------
+            Bool
+        """
         pass
 
     def log_results(self, logger: Logger, filename: Optional[str] = None) -> None:
+        """
+        Displays a log message for each incorrectly annotated function or method.
+        Parameters
+        ----------
+        logger : Logger
+            logger object that displays the message.
+        filename :
+            If provided, the filename will be prepended to the log message
+        Returns
+        -------
+            None
+        """
         prefix = ""
         if filename:
             prefix = f"{filename}: "
@@ -21,17 +39,27 @@ class Checker(ABC):
 
 
 class FunctionChecker(Checker):
+    """Checks if a function is correctly type-annotated."""
+
     def __init__(self, function: ast.FunctionDef, exclude_args: List[str] = ()) -> None:
         super().__init__()
         self.function: ast.FunctionDef = function
-        self.exclude_args = exclude_args
+        self.exclude_args = exclude_args  # TODO: move to parent class
 
     def check(self) -> bool:
+        """
+        Checks that the function is annotated (arguments and return type).
+        Returns
+        -------
+        bool
+            True if correctly annotated
+        """
         self.__check_args()
         self.__check_return()
-        return bool(self.errors)
+        return not bool(self.errors)
 
     def __check_args(self) -> None:
+        """Check that the arguments of a function are correctly type-annotated."""
         for argument in self.function.args.args:
             if not argument.annotation:
                 if argument.arg not in self.exclude_args:
@@ -41,6 +69,7 @@ class FunctionChecker(Checker):
                     )
 
     def __check_return(self) -> None:
+        """Check that the function return type is provided."""
         if not self.function.returns:
             self.errors.append(
                 f"Missing return annotation for function {self.function.name}, "
@@ -49,18 +78,29 @@ class FunctionChecker(Checker):
 
 
 class ClassChecker(Checker):
+    """
+    Checks if all methods in a given class are correctly type-annotated.
+    """
+
     def __init__(self, class_: ast.ClassDef, exclude_args: List[str] = ()) -> None:
         super().__init__()
         self.exclude_args = exclude_args
         self.class_ = class_
 
     def check(self) -> bool:
-        result = False
+        """
+        Checks if all methods in a given class are correctly type-annotated.
+        Returns
+        -------
+        bool
+            True if all methods are correctly type-annotated.
+        """
+        result = True
         for method in self.class_.body:
             if isinstance(method, ast.FunctionDef):
                 function_checker = FunctionChecker(
                     method, exclude_args=self.exclude_args
                 )
-                result = result or function_checker.check()
+                result = result and function_checker.check()
                 self.errors += function_checker.errors
         return result
--- annotation_checker/main.py
+++ annotation_checker/main.py
@@ -5,12 +5,16 @@ import ast
 
 from annotation_checker.checkers import FunctionChecker, ClassChecker
 
-logger = logging.getLogger("annotation-checker")
+logger = logging.getLogger("annotation_checker")
 logging.basicConfig()
 
 
 def check_annotated(file_list: List[str], exclude_self: bool = False) -> bool:
-    number_or_errors = 0
+    """
+    Iterates through the list of file paths, parses the files and checks if all functions and classes in the files are
+    correctly type-annotated.
+    """
+    result = True
     exclude_args = []  # TODO: add as argument
     if exclude_self:
         exclude_args.append("self")
@@ -25,18 +29,42 @@ def check_annotated(file_list: List[str], exclude_self: bool = False) -> bool:
                     checker = ClassChecker(item, exclude_args=exclude_args)
                 else:
                     continue
-                number_or_errors = number_or_errors + checker.check()
+                result = result and checker.check()
                 checker.log_results(logger)
-    return not bool(number_or_errors)
+    return result
+
+
+def parse_arguments() -> argparse.Namespace:
+    """
+    Parses command line arguments.
+    Returns
+    -------
+        Namespace
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "filenames", help="Files to be checked by annotation_checker.", nargs="+"
+    )
+    parser.add_argument(
+        "--strict",
+        help="If True and checks failes, exit the program with 1 code.",
+        type=bool,
+        default=False,
+    )
+    parser.add_argument(
+        "--exclude_self",
+        help="If True, omit type checking for parameter `self` in methods.",
+        type=bool,
+        default=False,
+    )
+    args = parser.parse_args()
+    return args
 
 
 def main() -> None:
+    """Reads the command lines arguments and runs the annotation_checker"""
     logger.setLevel(logging.INFO)
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filenames", nargs="+")
-    parser.add_argument("--strict", type=bool, default=False)
-    parser.add_argument("--exclude_self", type=bool, default=False)
-    args = parser.parse_args()
+    args = parse_arguments()
     logger.debug("Files: %s", args.filenames)
     exit_code = 1 - check_annotated(args.filenames, exclude_self=args.exclude_self)
     if args.strict and exit_code:
