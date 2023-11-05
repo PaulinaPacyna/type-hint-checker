1df4379141fa98138c3ec2d0fb83dac4669dd468
PaulinaPacyna <pacyna1998@gmail.com>
2023-11-05 16:25:07 +0100

    moved test cases to files

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import pathlib
 import pytest
@@ -8,36 +9,12 @@ from annotation_checker.exceptions import IncorrectFileException
 from annotation_checker.main import check_annotated, filter_files
 
 
-@fixture
-def mixed_args_with_return() -> str:
-    """Example of a function with a return type and some of the parameters are not
-    annotated"""
-    return "def f5(test_aa, bbb: int, ccc: 'Class') -> None:\n    pass"
-
-
-@fixture
-def mixed_args() -> str:
-    """Example of a function without return type and some of the parameters are not
-    annotated"""
-    return "def f1(a, b: int):\n    return 5"
-
-
-@fixture
-def no_return() -> str:
-    """Example of a function without a return type and annotated parameters"""
-    return "def f1(a: int):\n    pass"
-
-
-@fixture
-def no_args() -> str:
-    """Example of a function without arguments and with return type"""
-    return "def f1() -> None:\n    pass"
-
-
-@fixture
-def not_a_function() -> str:
-    """Should be omitted"""
-    return "x=4"
+NO_RETURN = "tests/cases/no_return.py"
+MIXED_ARGS = "tests/cases/mixed_args.py"
+NO_ARGS = "tests/cases/no_args.py"
+NOT_A_FUNCTION = "tests/cases/not_a_function.py"
+STRING_ANNOTATION = "tests/cases/string_annotation.py"
+MIXED_ARGS_WITH_RETURN = "tests/cases/mixed_args_with_return.py"
 
 
 @fixture
@@ -46,28 +23,19 @@ def incorrect_file() -> str:
     return "key1:\nkey2:"
 
 
-@fixture
-def string_annotation() -> str:
-    """Example of a function annotated with a string"""
-    return 'def f(self: "Class") -> int:\n    pass'
-
-
 @pytest.mark.parametrize(
-    "input_,result",
+    "input_path,result",
     [
-        ("mixed_args", False),
-        ("no_return", False),
-        ("no_args", True),
-        ("not_a_function", True),
-        ("string_annotation", True),
-        ("mixed_args_with_return", False),
+        (MIXED_ARGS, False),
+        (NO_RETURN, False),
+        (NO_ARGS, True),
+        (NOT_A_FUNCTION, True),
+        (STRING_ANNOTATION, True),
+        (MIXED_ARGS_WITH_RETURN, False),
     ],
 )
-def test_check_annotated(input_, result, tmp_path: pathlib.Path, request) -> None:
-    """Basic checks"""
-    file = tmp_path / "file.py"
-    file.write_text(request.getfixturevalue(input_), encoding="utf-8")
-    assert check_annotated([file]) == result
+def test_check_annotated(input_path, result) -> None:
+    assert check_annotated([input_path]) == result
 
 
 def test_incorrect_file(incorrect_file, tmp_path: pathlib.Path) -> None:
@@ -95,13 +63,11 @@ def test_filter_files_no_exclude() -> None:
     assert filter_files(file_list, pattern) == result
 
 
-def test_filepath_in_log(no_return, tmp_path: pathlib.Path, caplog) -> None:
+def test_filepath_in_log(caplog) -> None:
     """Test if the path to th file appears in the log"""
-    file = tmp_path / "file737ny73466364781.py"
-    file.write_text(no_return, encoding="utf-8")
     with caplog.at_level(logging.INFO):
-        check_annotated([file])
-    assert "file737ny73466364781.py" in caplog.text
+        check_annotated([NO_RETURN])
+    assert NO_RETURN in caplog.text
 
 
 @pytest.mark.parametrize(
@@ -111,49 +77,34 @@ def test_filepath_in_log(no_return, tmp_path: pathlib.Path, caplog) -> None:
         ("", False),
     ],
 )
-def test_exclude_parameters_by_regex(
-    pattern, result, mixed_args_with_return, tmp_path: pathlib.Path
-):
+def test_exclude_parameters_by_regex(pattern, result):
     """Test if parameters are excluded"""
-    file = tmp_path / "file.py"
-    file.write_text(mixed_args_with_return, encoding="utf-8")
-    assert check_annotated([file], exclude_parameters=pattern) == result
+    assert (
+        check_annotated([MIXED_ARGS_WITH_RETURN], exclude_parameters=pattern) == result
+    )
 
 
 @pytest.mark.parametrize(
-    "input_,pattern,result",
+    "input_file,pattern,result",
     [
-        ("mixed_args", "", False),
-        ("no_return", "", False),
-        ("mixed_args_with_return", "", False),
-        ("mixed_args", "dgag", False),
-        ("no_return", "adsg", False),
-        ("mixed_args_with_return", "agddfa", False),
-        ("mixed_args", "^f", True),
-        ("no_return", "^f", True),
-        ("mixed_args_with_return", "^f", True),
+        (MIXED_ARGS, "", False),
+        (NO_RETURN, "", False),
+        (MIXED_ARGS_WITH_RETURN, "", False),
+        (MIXED_ARGS, "dgag", False),
+        (NO_RETURN, "adsg", False),
+        (MIXED_ARGS_WITH_RETURN, "agddfa", False),
+        (MIXED_ARGS, "^f", True),
+        (NO_RETURN, "^f", True),
+        (MIXED_ARGS_WITH_RETURN, "^f", True),
     ],
 )
-def test_exclude_by_name(input_, pattern, result, tmp_path: pathlib.Path, request):
+def test_exclude_by_name(input_file, pattern, result):
     """Test excluding functions and classes by name"""
-    file = tmp_path / "file.py"
-    file.write_text(request.getfixturevalue(input_), encoding="utf-8")
-    assert check_annotated([file], exclude_by_name=pattern) == result
+    assert check_annotated([input_file], exclude_by_name=pattern) == result
 
 
-def testing_multiple_files(
-    mixed_args, no_return, mixed_args_with_return, tmp_path: pathlib.Path, caplog
-):
-    files_content_mapping = {
-        "mixed_args.py": mixed_args,
-        "no_return.py": no_return,
-        "mixed_args_with_return.py": mixed_args_with_return,
-    }
-    files = []
-    for case, content in files_content_mapping.items():
-        file = tmp_path / case
-        file.write_text(content, encoding="utf-8")
-        files.append(file)
+def testing_multiple_files(caplog):
+    file_list = [MIXED_ARGS, NO_RETURN, MIXED_ARGS_WITH_RETURN]
     with caplog.at_level(logging.INFO):
-        check_annotated(list(files))
-        assert all(filename in caplog.text for filename in files_content_mapping)
+        check_annotated(file_list)
+        assert all(filename in caplog.text for filename in file_list)
