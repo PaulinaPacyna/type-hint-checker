2581d9c6892ce0cd51a2da3f8629e93cf2fffdb9
PaulinaPacyna <pacyna1998@gmail.com>
2023-11-19 18:28:56 +0100

    shell false

--- tests/test_pre_commit.py
+++ tests/test_pre_commit.py
@@ -3,18 +3,17 @@ import subprocess
 
 
 def run_command(command: str) -> subprocess.CompletedProcess:
+    """Common interface for executing command line programms"""
     shell_path = os.getenv("ANNOTATION_CHECKER_SHELL_PATH")
     if shell_path:
         command = f'{shell_path}, -c "{command}"'
     return subprocess.run(
-        command,
-        capture_output=True,
-        universal_newlines=True,
-        shell=False,
+        command, capture_output=True, universal_newlines=True, check=False
     )
 
 
 def test_run_all() -> None:
+    """Basic check"""
     process = run_command(
         "pre-commit run annotation_checker --all-files -c tests/configs/strict.yaml"
     )
@@ -22,22 +21,27 @@ def test_run_all() -> None:
     lines = [line.strip() for line in lines]
     assert set(lines) == {
         "",
-        "annotation_checker.......................................................Failed",
+        "annotation_checker......................................................."
+        "Failed",
         "- hook id: annotation_checker",
         "- exit code: 1",
-        "INFO:annotation_checker:tests/cases/mixed_args.py: Missing annotation for argument a "
+        "INFO:annotation_checker:tests/cases/mixed_args.py: Missing annotation for "
+        "argument a "
         "(function f1), line 1",
         "INFO:annotation_checker:tests/cases/mixed_args.py: Missing return annotation "
         "for function f1, line 1",
-        "INFO:annotation_checker:tests/cases/no_return.py: Missing return annotation for "
+        "INFO:annotation_checker:tests/cases/no_return.py: Missing return annotation "
+        "for "
         "function f1, line 1",
-        "INFO:annotation_checker:tests/cases/mixed_args_with_return.py: Missing annotation for "
+        "INFO:annotation_checker:tests/cases/mixed_args_with_return.py: Missing "
+        "annotation for "
         "argument test_aa (function f5), line 1",
     }
     assert process.returncode == 1
 
 
 def test_run_all_not_strict() -> None:
+    """Test if pre-commit doesn't fail when strict=False"""
     process = run_command(
         "pre-commit run annotation_checker --all-files -c tests/configs/not-strict.yaml"
     )
