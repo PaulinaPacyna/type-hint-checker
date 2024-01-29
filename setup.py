b9b0fc4765ca76b676f7bce9d40cc136d91de63d
PaulinaPacyna <pacyna1998@gmail.com>
2024-01-29 23:25:47 +0100

    keep version in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as readme:
-    with open("VERSION", "r", encoding="utf-8") as version:
-        setup(long_description=readme.read(), version=version.read().strip("\n"))
+    setup(long_description=readme.read())
